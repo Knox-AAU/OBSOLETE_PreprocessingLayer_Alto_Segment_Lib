333f7b003bf7ed37a9612719afb2d5e72b177fc0
Unknown <durumguru@gmail.com>
2020-11-04 08:57:42 +0100

    Refactored example

--- example.py
+++ example.py
@@ -16,7 +16,7 @@ filepath: str
 filetype = ".jp2"
 
 
-def displaySegments(segments):
+def display_segments(display_segments):
     #plt.imshow(Image.open("/home/tlorentzen/Desktop/Example/1942/aalborgstiftstidende-1942-01-02-01-0028B.tiff"))
     plt.imshow(Image.open(filepath+filetype))
     plt.rcParams.update({'font.size': 3, 'text.color': "red", 'axes.labelcolor': "red"})
@@ -26,7 +26,7 @@ def displaySegments(segments):
     # Add the patch to the Axes
     #plt.hlines(100, 100, 100+repair.get_median_column_width(), colors='k', linestyles='solid', label='Median paragraph width')
 
-    for segment in segments:
+    for segment in display_segments:
         plt.gca().add_patch(Rectangle((segment.pos_x, segment.pos_y), (segment.lower_x-segment.pos_x), (segment.lower_y-segment.pos_y), linewidth=0.3, edgecolor='r', facecolor='none'))
         # plt.text(segment.pos_x+25, segment.pos_y+30, "["+str(counter)+"]", horizontalalignment='left', verticalalignment='top')
         # plt.text(seg[0]+45, seg[1] + 200, str((seg[2]-seg[0])), horizontalalignment='left', verticalalignment='top')
@@ -78,4 +78,4 @@ if __name__ == '__main__':
 
     segmentOrder = SegmentOrdering(base_path, filename)
     segmentsInArticles = segmentOrder.distributeSegmentsIntoArticles(segments_headers, segments_para)
-    displaySegments(segments)
+    display_segments(segments)
