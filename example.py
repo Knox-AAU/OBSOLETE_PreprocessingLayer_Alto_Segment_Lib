513a63c6dc8a76cc3fb02641be0e5a725c7b4e47
Alex <Alexik1998@gmail.com>
2020-11-10 14:11:44 +0100

    Fjerner fejl ved run

--- example.py
+++ example.py
@@ -79,11 +79,11 @@ if __name__ == '__main__':
     #    print('x:{0} y:{1} x1:{2} y2:{3} - {4}'.format(str(seg.x1), str(seg.y1), str(seg.x2), str(seg.y2), seg.type))
 
     # segments = segmenter.extract_segments()
-    # segments_para = [segment for segment in segments if segment.type == "paragraph"]
+    segments_para = [segment for segment in segments if segment.type == "paragraph"]
     segments_headers = [segment for segment in segments if segment.type == "headline"]
 
     segment_order = SegmentOrdering(base_path, filename)
-    segments_in_articles = segment_order.distribute_segments_into_articles(segments_headers, segments_para)
+    # segments_in_articles = segment_order.distribute_segments_into_articles(segments_headers, segments_para)
     # display_segments(segments_headers)
 
     # Display lines
