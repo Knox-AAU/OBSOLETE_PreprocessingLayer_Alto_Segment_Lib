feee8b6f0cc10f7d35cbe511995548d7a1ff10cf
Unknown <durumguru@gmail.com>
2020-11-12 15:05:00 +0100

    Fixed multi run

--- example.py
+++ example.py
@@ -20,8 +20,8 @@ filepath: str
 filetype = ".jp2"
 
 
-def display_segments(segments_for_display, name):
-    plt.imshow(Image.open(filepath + filetype))
+def display_segments(segments_for_display, file_path, name):
+    plt.imshow(Image.open(file_path + filetype))
     plt.rcParams.update({'font.size': 3, 'text.color': "red", 'axes.labelcolor': "red"})
 
     counter = 1
@@ -37,12 +37,12 @@ def display_segments(segments_for_display, name):
         # plt.text(seg[0]+45, seg[1] + 200, str((seg[2]-seg[0])), horizontalalignment='left', verticalalignment='top')
         counter += 1
 
-    plt.savefig(filepath + "-" + name + ".png", dpi=600, bbox_inches='tight')
+    plt.savefig(file_path + "-" + name + ".png", dpi=600, bbox_inches='tight')
     plt.gca().clear()
 
 
-def display_lines(headers_for_display, paragraphs_for_display, name):
-    plt.imshow(Image.open(filepath + filetype))
+def display_lines(headers_for_display, paragraphs_for_display, file_path, name):
+    plt.imshow(Image.open(file_path + filetype))
     plt.rcParams.update({'font.size': 3, 'text.color': "red", 'axes.labelcolor': "red"})
 
     counter = 1
@@ -59,13 +59,13 @@ def display_lines(headers_for_display, paragraphs_for_display, name):
                       edgecolor='r', facecolor='none'))
         counter += 1
 
-    plt.savefig(filepath + "-" + name + ".png", dpi=600, bbox_inches='tight')
+    plt.savefig(file_path + "-" + name + ".png", dpi=600, bbox_inches='tight')
     plt.gca().clear()
 
 
-def run_multiple_files(base_path):
-    for file_name in os.listdir(base_path):
-        file_path = os.path.join(base_path, file_name)
+def run_multiple_files(basepath):
+    for file_name in os.listdir(basepath):
+        file_path = os.path.join(basepath, file_name)
 
         if not os.path.isfile(file_path) or not file_path.endswith(".jp2"):
             continue
@@ -77,7 +77,7 @@ def run_multiple_files(base_path):
 
 def run_file(file_path):
     lines = LineExtractor().extract_lines_via_path(file_path + ".jp2")
-    #display_lines([], lines, "streger")
+    #display_lines([], lines, file_path, "streger")
 
     altoExtractor = AltoSegmentExtractor(file_path + ".alto.xml")
     altoExtractor.set_dpi(300)
@@ -88,16 +88,16 @@ def run_file(file_path):
     text_lines = altoExtractor.extract_lines()
     text_lines = segment_helper.repair_text_lines(text_lines, lines)
     lists = segment_helper.group_lines_into_paragraphs_headers(text_lines)
-    #display_lines(lists[0], lists[1], "lines")
+    #display_lines(lists[0], lists[1], "lines", file_path)
     segments = segment_helper.combine_lines_into_segments(lists[1])
-    #display_segments(segments, "segments")
+    #display_segments(segments, file_path, "segments")
 
     paragraphs = [segment for segment in segments if segment.type == "paragraph"]
     repair = RepairSegments(paragraphs, 30)
     rep_rows_segments2 = repair.repair_rows()
     paragraphs.clear()
     segments_para = rep_rows_segments2
-    display_segments(segments_para, "repaired")
+    display_segments(segments_para, file_path, "repaired")
 
 
 if __name__ == '__main__':
@@ -111,5 +111,5 @@ if __name__ == '__main__':
     filename = args.filename
     filepath = base_path + filename
 
-    # run_multiple_files(base_path)
-    run_file(filepath)
+    run_multiple_files(base_path)
+    # run_file(filepath)
